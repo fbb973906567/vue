36a4e04944251abbb1e937b9c7196cfbcce7cf3a
fanhaipeng <973906567@qq.com>
2019-11-06 22:41:19 +0800

    2

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,41 +2,26 @@
   <div class="hello">
     <el-container>
       <el-aside>
-        <el-menu default-active="2" class="el-menu-vertical-demo">
+        <el-menu default-active="2" class="el-menu-vertical-demo" router>
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>导航一</span>
             </template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-            <el-menu-item index="1-4-2">选项1</el-menu-item>
+            <el-menu-item index="home">Home</el-menu-item>
+            <el-menu-item index="application">application</el-menu-item>
             <el-menu-item index="1-4-4">选项1</el-menu-item>
           </el-submenu>
-          <el-menu-item index="2">
+          <el-menu-item index="config">
             <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
+            <span slot="title">config</span>
           </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-submenu index="4">
+          <el-submenu index="3">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航四</span>
+              <span>导航三</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="4-1">选项1</el-menu-item>
-              <el-menu-item index="4-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="4-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="4-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="4-4-1">选项1</el-menu-item>
-            </el-submenu>
+            <el-menu-item index="index">Helloworld</el-menu-item>
           </el-submenu>
         </el-menu>
       </el-aside>
--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,31 @@ import VueRouter from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import Application from '../components/Application.vue'
 import Config from '../components/Config.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-  mode: 'history',
-  routes:[{
-    path:'/',
-    name:'HelloWorld',
-    component:HelloWorld
-  },{
-    path:'/index',
-    name:'HelloWorld',
-    component:HelloWorld
-  },{
-    path:'/application',
-    name:'Application',
-    component:Application
-  },{
-    path:'/config',
-    name:'Config',
-    component:Config
-  }]
+    mode: 'history',
+    routes: [{
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+    }, {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    }, {
+        path: '/index',
+        name: 'HelloWorld',
+        component: HelloWorld
+    }, {
+        path: '/application',
+        name: 'Application',
+        component: Application
+    }, {
+        path: '/config',
+        name: 'Config',
+        component: Config
+    }]
 })
